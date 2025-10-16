8d260d9908c055e59cd21249f41b692d5f5d9451
TRUEMKTUS <truemkt.us@gmail.com>
2025-10-16 00:24:07 -0500

    Update download-folder.js

--- api/download-folder.js
+++ api/download-folder.js
@@ -8,6 +8,17 @@ const BUCKET = 'VIDEO-BUCKET';
 const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 export default async function handler(req, res) {
+    // Agregar headers CORS
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
+
     if (req.method !== 'POST') {
         return res.status(405).json({ error: 'Method not allowed' });
     }
